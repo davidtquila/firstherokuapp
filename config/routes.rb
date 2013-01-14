0febfafb0a12056ac80e439eb79b90e941fab7be
davidtquila <david@tquila.com>
2013-01-14 00:54:34 +0000

    18 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
-OmniauthDemo::Application.routes.draw do
-
-  get "accounts/index"
-  post "accounts/search"
-  post "accounts/create"
-  match "accounts/:id", :to => "accounts#show"
-  match "accounts/:id/edit", :to => "accounts#edit"
-  match "accounts/:id/save", :to => "accounts#save"
-  match "accounts/:id/new_opp", :to => "accounts#new_opp"
-  match "accounts/:id/save_opp", :to => "accounts#save_opp"
-  
-  get "sessions/create"
-  
-  #add our oauth redirect route - qw
-  match '/auth/:provider/callback', :to => 'sessions#create'
-  match '/auth/failure', :to => 'sessions#fail'
-
+Demo1::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -64,11 +48,11 @@ OmniauthDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id))(.:format)'
 end
